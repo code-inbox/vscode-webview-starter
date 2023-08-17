47960ed1d6a264e4999f3d7f343a801c19b2d475
Matt Oxley <matt_ox@hotmail.com>
2023-08-17 23:31:48 +0200

    env

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,6 +1,7 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'cm8ts1',
     chromeWebSecurity: false,
     e2e: {
         setupNodeEvents(on, config) {
