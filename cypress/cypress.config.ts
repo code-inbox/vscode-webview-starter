e65fad37607c6dd0103d6e717be97263186cf208
Matt Oxley <matt_ox@hotmail.com>
2023-08-18 23:00:32 +0200

    fix

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -3,6 +3,7 @@ const {defineConfig} = require('cypress')
 module.exports = defineConfig({
     projectId: 'cm8ts1',
     chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
