5450d51dadac76eacf5bcb2681657f807788727d
Matt Oxley <matt_ox@hotmail.com>
2023-08-19 15:18:18 +0200

    test

--- packages/vscode-scripts/frameworks/react.js
+++ packages/vscode-scripts/frameworks/react.js
@@ -1,5 +1,5 @@
 import { reactMountPlugin, getStaticInfoPlugin } from "../plugins.js"
 
 export default {
-  plugins: [reactMountPlugin(), getStaticInfoPlugin()],
+  plugins: [getStaticInfoPlugin()],
 }
