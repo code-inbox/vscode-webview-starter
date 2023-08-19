52b73699aec1735e18e2c1d59c972318246c3cf4
Matthew Oxley <matt_ox@hotmail.com>
2023-08-19 19:36:19 +0200

    Cleanup Cypress (#4)
    * Cleanup Cypress
    
    * restore

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,29 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 declare namespace Cypress {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Chainable<Subject> {
@@ -58,34 +32,9 @@ Cypress.Commands.add(
     }
 );
 
-Cypress.Commands.add(
-    'getWebviews', () => {
-        // returns all iframes with class '.webview.ready
-        // log the number of "body" elements
-        return cy.get('.monaco-workbench').find('iframe');
-    }
-)
-
-
-Cypress.Commands.add('getIframeBody', () => {
-    // get the iframe > document > body
-    // and retry until the body element is not empty
-    cy.log('getIframeBody')
-
-    return cy
-        .get('.monaco-workbench > div > iframe', {log: false}).should('have.length', 2)
-        .its('0.contentDocument.body', {log: false}).should('not.be.empty')
-        .then((body) => cy.wrap(body, {log: false}))
-        .its('0', {log: false}).invoke('querySelector', 'iframe#active-frame').should('not.be.null')
-        .then(iframe => cy.wrap(iframe, {log: false}))
-        .its('0.contentDocument.body', {log: false}).should('not.be.empty')
-        .then((body) => cy.wrap(body, {log: false}))
-        .its('0', {log: false}).invoke('querySelector', 'button').should('not.be.null') // todo check for emptiness
-})
-
 Cypress.Commands.add('ensureViewContainerActive', () => {
-    cy.get('.composite-bar .actions-container .action-item').its('0')
-        .then(item => cy.wrap(item).click())
+    cy.get('.composite-bar .actions-container .action-item')
+        .its('0').then(item => cy.wrap(item).click())
         .then(() => {
             cy.get('.composite-bar .actions-container .action-item').should('have.length', 6).its('5')
                 .then(item => cy.wrap(item).click())
@@ -97,11 +46,5 @@ Cypress.Commands.add('iframeOnload', {prevSubject: 'element'}, $iframe => {
         $iframe.on('load', () => {
             resolve($iframe.contents().find('body'));
         });
-    });
-});
-
-Cypress.Commands.add('iframeDirect', {prevSubject: 'element'}, $iframe => {
-    return new Cypress.Promise(resolve => {
-        resolve($iframe.contents().find('body'));
     });
 });
