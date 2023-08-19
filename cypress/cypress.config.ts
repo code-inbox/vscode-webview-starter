008ee91376470675fdbc4c48f2c1e9e0fc7a509e
Matt Oxley <matt_ox@hotmail.com>
2023-08-19 19:31:45 +0200

    Cleanup Cypress

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,14 +1,9 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-    projectId: 'cm8ts1',
     chromeWebSecurity: false,
     defaultCommandTimeout: 4000,
     e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-        testIsolation: false,
         record: true,
     },
 })
